638411629f5517ff45f40b5b0c4e042352d21be8
Lev Rednikov <120374210+CheateRYT@users.noreply.github.com>
2024-05-01 23:53:36 +0300

    fix: Add try catch for users contoller

---
+++ routes/middleware/auth.js
@@ -0,0 +1,11 @@
+const jwt = require("jsonwebtoken");
+const { prisma } = require("@prisma/client");
+
+const auth = async (req, res, next) => {
+  try {
+  } catch {}
+};
+
+modulemodule.exports = {
+  auth,
+};
