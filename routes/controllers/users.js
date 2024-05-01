bf5cefcc7c61a571de53f0396c24f59f1e6a96bd
Lev Rednikov <120374210+CheateRYT@users.noreply.github.com>
2024-05-01 18:39:35 +0300

    feat: Added login logic

--- routes/controllers/users.js
+++ routes/controllers/users.js
@@ -1,3 +1,13 @@
+const { prisma } = require("../../prisma/prisma-client");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+/**
+ *
+ * @route POST /api/user/login
+ * @desc Логин
+ * @access Public
+ */
 const login = async (req, res) => {
   const { email, password } = req.body;
 
@@ -5,6 +15,19 @@ const login = async (req, res) => {
     return res
       .status(400)
       .json({ message: "Пожалуйста, заполните обязательные поля" });
+  }
+  const user = await prisma.user.findFirst({
+    where: {
+      email,
+    },
+  });
+  const isPasswordCorrect =
+    user && (await bcrypt.compare(password, user.password));
+
+  if (user && isPasswordCorrect) {
+    res.status(200).json({ id: user.id, email: user.email, name: user.email });
+  } else {
+    return res.status(400).json({ message: "Неверно введен логин или пароль" });
   }
 };
 const register = async (req, res) => {
