140d35a0dc86b1660df1ab34da3cc4a88ecb091a
Lev Rednikov <120374210+CheateRYT@users.noreply.github.com>
2024-05-06 21:06:24 +0300

    feat: Add midleware for Get current user function

--- routes/controllers/users.js
+++ routes/controllers/users.js
@@ -91,8 +91,14 @@ const register = async (req, res) => {
   }
 };
 
+/**
+ *
+ * @route GET /api/user/current
+ * @desc Текущий пользователь
+ * @access Private
+ */
 const current = async (req, res) => {
-  res.send("current");
+  return res.status(200).json(req.user);
 };
 
 module.exports = {
--- routes/middleware/auth.js
+++ routes/middleware/auth.js
@@ -3,9 +3,21 @@ const { prisma } = require("@prisma/client");
 
 const auth = async (req, res, next) => {
   try {
-  } catch {}
+    let token = req.headers.authorization?.split(" ")[1];
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: decodedToken.id,
+      },
+    });
+    req.user = user;
+  } catch (error) {
+    res.status(401).json({ message: "Не авторизован" });
+  }
 };
 
-modulemodule.exports = {
+module.exports = {
   auth,
 };
