af73b79763d4d964e9c0a2d07ec22c9b876bde72
Lev Rednikov <120374210+CheateRYT@users.noreply.github.com>
2024-05-31 22:13:20 +0300

    Add login form

--- client/src/components/pages/login/Login.tsx
+++ client/src/components/pages/login/Login.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import Layout from "../../Layout";
-import { Card, Form, Row } from "antd";
+import {Card, Form, Row, Space, Typography} from "antd";
+import CustomInput from "../../CustomInput";
+import PasswordInput from "../../PasswordInput";
+import CustomButton from "../../CustomButton";
+import {Link} from "react-router-dom";
+import {Paths} from "../../../path";
 
 const Login = () => {
   return (
     <Layout>
       <Row align="middle" justify="center">
         <Card title="Войдите" style={{ width: "30rem" }}>
-          <Form onFinish={() => null}> </Form>
-        </Card>
+          <Form onFinish={() => null}>
+              <CustomInput type={"email"} name={"email"} placeholder={"Email"} />
+              <PasswordInput name={"password"} placeholder={"Пароль"} />
+              <CustomButton type={"primary"} htmlType={"submit"}>Войти</CustomButton>
+          </Form>
+            <Space direction={"vertical"} size={"large"}>
+                <Typography.Text>Нет аккаунта? <Link to={Paths.register}>Зарегистрируйтесь</Link></Typography.Text>
+            </Space>
+             </Card>
       </Row>
     </Layout>
   );
